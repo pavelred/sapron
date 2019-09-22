88035e2baf566241540e3ac90f6f90009a144527
Pavel <pavel.redwood@gmail.com>
2019-09-22 20:28:58 +0300

    Created new text for Yandex Market without dropdown

--- ConvertedJSFiles/pageObject/geoPageObject.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const protractor_1 = require("protractor");
-exports.geoPageYan = {
-    city: protractor_1.element(protractor_1.by.css("#city__front-input")),
-    dropdownFirstEl: protractor_1.element(protractor_1.by.xpath("//li[1]"))
-};
